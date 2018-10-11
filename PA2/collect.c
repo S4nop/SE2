c6571dc51917445f2ca465dc36c6dc10207c40a8
S4nop <rshtiger@naver.com>
2018-10-11 21:49:32 +0900

    Edit a little
    Signed-off-by: S4nop <rshtiger@naver.com>

--- PA2/collect.c
+++ PA2/collect.c
@@ -21,11 +21,74 @@
 
 void collect (void);
 
+static char *getcmd() {
+    static char buf[256];
+
+    write(1, "> ", 2);
+    if (read(0, buf, sizeof(buf)) <= 0) return NULL;
+
+    if (buf[strlen(buf)] == '\n')
+        buf[strlen(buf)] = 0;
+
+    return buf;
+}
+
+int parsecmd(char *buf, char **argv) 
+{
+    char *delim;         /* Points to first space delimiter */
+    int argc;            /* Number of args */
+    int bg;              /* Background job? */
+
+    buf[strlen(buf)-1] = ' ';  /* Replace trailing '\n' with space */
+    while (*buf && (*buf == ' ')) /* Ignore leading spaces */
+	buf++;
+
+    /* Build the argv list */
+    argc = 0;
+    while ((delim = strchr(buf, ' '))) {
+	argv[argc++] = buf;
+	*delim = '\0';
+	buf = delim + 1;
+	while (*buf && (*buf == ' ')) /* Ignore spaces */
+	       buf++;
+    }
+    argv[argc] = NULL;
+    
+    if (argc == 0)  /* Ignore blank line */
+	return 1;
+
+    /* Should the job run in the background? */
+    if ((bg = (*argv[argc-1] == '&')) != 0)
+	argv[--argc] = NULL;
+
+    return bg;
+}
+
+static char *getcmd() {
+    static char buf[256];
+
+    fputs("> ", stdout); fflush(stdout);
+    if (fgets(buf, sizeof(buf), stdin) == NULL)
+        return NULL;
+
+    if (buf[strlen(buf)] == '\n')
+        buf[strlen(buf)] = 0;
+
+    return buf;
+}
+
 int main (int argc, char* argv[]) {
     collect();
     return 0;
 }
 
 void collect (void){
-
+    char *cmdLine;
+    char tmp[256];
+    cmdLine = getcmd();
+    strncpy(tmp, cmdLine, 7);
+    if(!strcmp(tmp, "http://") || !strcmp(tmp, "https:/")){
+	
+    }
+    printf("%s\n", cmdLine);
 }
